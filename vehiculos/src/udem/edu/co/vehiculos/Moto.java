4ae777d630eec14b507248edc626cb3cd4e6070c
alejou2009 <alejou2009@gmail.com>
2021-09-02 06:28:49 -0500

    Primer avance, creacion projecto.

---
+++ vehiculos/src/udem/edu/co/vehiculos/Moto.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package udem.edu.co.vehiculos;
+
+/**
+ *
+ * @author s107e11
+ */
+public class Moto {
+    
+    private String placa;
+    private String marca;
+
+    public Moto(String placa, String marca) {
+        this.placa = placa;
+        this.marca = marca;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+}
